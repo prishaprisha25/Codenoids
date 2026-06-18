d7eb40f5d4638118ef313131f30a89c62026ac87
prishaprisha25 <prisha1223133@jmit.ac.in>
2026-06-18 20:21:09 +0530

    Create Container With Most Water.cpp

---
+++ day2/Container With Most Water.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int maxArea(std::vector<int>& height) {
+        int max_water = 0;
+        int left = 0;
+        int right = height.size() - 1;
+        
+        while (left < right) {
+            int width = right - left;
+            
+            int current_height = std::min(height[left], height[right]);
+            
+            int current_water = width * current_height;
+            max_water = std::max(max_water, current_water);
+            
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return max_water;
+    }
+};
